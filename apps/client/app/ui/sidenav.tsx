9b265b301193b636ee9fcbd041ae2ae7d81e6395
Patrick Glynn <pglynnco@gmail.com>
2024-10-24 15:19:46 -0600

    styling logo component

--- apps/client/app/ui/sidenav.tsx
+++ apps/client/app/ui/sidenav.tsx
@@ -1,13 +1,19 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/nav-links';
+import Logo from '@/app/ui/logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40" href="/">
-        <div className="w-32 text-white md:w-40">{/* <AcmeLogo /> */}</div>
+      <Link
+        className="mb-2 flex h-20 items-center justify-center rounded-md border-2 border-my-blue rounded p-4 md:h-40"
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40">
+          <Logo />
+        </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
