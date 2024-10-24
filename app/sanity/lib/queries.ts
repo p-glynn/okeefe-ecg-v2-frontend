f581af193e1cfea835953b77acf6ec60ec155759
Patrick Glynn <pglynnco@gmail.com>
2024-10-24 14:31:42 -0600

    move to monorepo project structure using turbo

--- app/sanity/lib/queries.ts
+++
@@ -1,3 +0,0 @@
-import { defineQuery } from 'next-sanity';
-
-export const ECGS_QUERY = defineQuery(`*[_type=="ecg"]{_id, title}`);
--- app/ui/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Roboto_Flex } from 'next/font/google';
-
-export const robotoFlex = Roboto_Flex({
-  subsets: ['latin'],
-});
