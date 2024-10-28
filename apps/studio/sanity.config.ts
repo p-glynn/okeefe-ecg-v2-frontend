32da979b638408b05ff611b64c862a4e120fe1b3
Patrick Glynn <pglynnco@gmail.com>
2024-10-28 16:13:09 -0600

    temp / test

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,28 +1,28 @@
-import {defineConfig} from 'sanity';
-import {structureTool} from 'sanity/structure';
-import {media} from 'sanity-plugin-media';
-import {visionTool} from '@sanity/vision';
-import {schemaTypes} from './schemaTypes';
-import {structure} from './structure';
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { media } from 'sanity-plugin-media';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './schemaTypes';
+import { structure } from './structure';
 
 export default defineConfig({
-    name: 'default',
-    title: "O'Keefe ECG",
+  name: 'default',
+  title: "O'Keefe ECG",
 
-    projectId: '3g00hitf',
-    dataset: 'production',
+  projectId: '3g00hitf',
+  dataset: 'production',
 
-    plugins: [
-        structureTool({
-            name: 'questions',
-            title: 'Questions',
-            structure,
-        }),
-        visionTool(),
-        media(),
-    ],
+  plugins: [
+    structureTool({
+      name: 'questions',
+      title: 'All Questions',
+      structure,
+    }),
+    visionTool(),
+    media(),
+  ],
 
-    schema: {
-        types: schemaTypes,
-    },
+  schema: {
+    types: schemaTypes,
+  },
 });
