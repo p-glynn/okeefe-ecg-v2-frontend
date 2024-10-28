e6fb4b4dbb328d80636660be2662e147aa219c8f
Patrick Glynn <pglynnco@gmail.com>
2024-10-28 12:52:44 -0600

    temp comment out search to test deploy

--- apps/client/app/questions/page.tsx
+++ apps/client/app/questions/page.tsx
@@ -1,6 +1,6 @@
 // import { Suspense } from 'react';
 import { robotoFlex } from '@/app/ui/fonts';
-import Search from '../ui/search';
+// import Search from '../ui/search';
 import { client } from '@/app/sanity/lib/client';
 import { fetchQuestionsByTypeQuery } from '@/app/sanity/lib/queries';
 import { Question } from '@/app/lib/definitions';
@@ -11,9 +11,7 @@ export default async function Page() {
   return (
     <main>
       <h1 className={`${robotoFlex} mb-4 text-xl md:text-2xl`}>Questions</h1>
-      <div className="mb-4">
-        <Search placeholder="Enter a search term..." />
-      </div>
+      <div className="mb-4">{/* <Search placeholder="Enter a search term..." /> */}</div>
       <ul>
         {questions.map((question: Question) => (
           <li key={question._id}>
