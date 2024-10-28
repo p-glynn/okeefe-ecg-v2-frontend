5b9404cdfeefe220e13083f0a261953a2c0e0fc1
Patrick Glynn <pglynnco@gmail.com>
2024-10-28 11:06:51 -0600

    add search component

--- apps/client/app/questions/page.tsx
+++ apps/client/app/questions/page.tsx
@@ -1,15 +1,19 @@
 // import { Suspense } from 'react';
 import { robotoFlex } from '@/app/ui/fonts';
+import Search from '../ui/search';
 import { client } from '@/app/sanity/lib/client';
 import { fetchQuestionsByTypeQuery } from '@/app/sanity/lib/queries';
 import { Question } from '@/app/lib/definitions';
 
 export default async function Page() {
   const questions = await client.fetch(fetchQuestionsByTypeQuery('ecg'));
-  console.log(questions);
+
   return (
     <main>
       <h1 className={`${robotoFlex} mb-4 text-xl md:text-2xl`}>Questions</h1>
+      <div className="mb-4">
+        <Search placeholder="Enter a search term..." />
+      </div>
       <ul>
         {questions.map((question: Question) => (
           <li key={question._id}>
