e1603d0cea5025eb399c554b6b442f350198257f
Patrick Glynn <pglynnco@gmail.com>
2024-10-29 13:39:10 -0600

    initial commit

---
+++ packages/config/index.ts
@@ -0,0 +1,7 @@
+import tailwindConfig from './tailwind.config';
+import tsconfig from './tsconfig.json';
+
+export default {
+  tailwind: tailwindConfig,
+  tsconfig: tsconfig,
+};
---
+++ packages/config/tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        'old-blue': '#24438e',
+        'old-dark-blue': '#133283',
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
+      },
+    },
+  },
+};
+
+export default config;
