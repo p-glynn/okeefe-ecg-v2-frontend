7d92eb2079f1c5c9563d161ecc8092ab5910b442
Patrick Glynn <pglynnco@gmail.com>
2024-10-30 14:00:49 -0600

    update tailwind content

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -3,7 +3,11 @@ import sharedConfig from '@okeefe-ecg-v2-frontend/config-tailwind';
 
 const config: Pick<Config, 'content' | 'presets'> = {
   presets: [sharedConfig],
-  content: ['./**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
 };
 
 export default config;
