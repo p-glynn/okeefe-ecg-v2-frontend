7a581461ee1fae9222b114b9a2b4d549dd6f304b
Patrick Glynn <pglynnco@gmail.com>
2024-10-30 13:40:49 -0600

    import & use shared tailwind config

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,29 +1,9 @@
 import type { Config } from 'tailwindcss';
+import sharedConfig from '@okeefe-ecg-v2-frontend/config-tailwind';
 
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        'my-blue': '#24438e',
-        'my-dark-blue': '#133283',
-      },
-    },
-    keyframes: {
-      shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
-        },
-      },
-    },
-  },
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require('@tailwindcss/forms')],
+const config: Pick<Config, 'content' | 'presets'> = {
+  presets: [sharedConfig],
+  content: ['./**/*.{js,ts,jsx,tsx,mdx}'],
 };
+
 export default config;
